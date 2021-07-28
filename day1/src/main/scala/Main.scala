1bb70b3a13764bb04e0b3a8a2b79396ac62dd08f
Ari Fordsham <arifordsham@gmail.com>
2021-07-28 15:52:39 +0100

    Refactor

--- day1/src/main/scala/Main.scala
+++ day1/src/main/scala/Main.scala
@@ -1,24 +1,24 @@
 import scala.io.Source
 
-def product[A, B](xs : List[A], ys : List[B]) =
-  xs.map(x => ys.map(y => (x, y))).flatten
+def cross[A](xs : List[List[A]], ys : List[List[A]]) =
+  xs.map(x => ys.map(y => x ++ y)).flatten
+
+def solve(xs : List[List[Int]]) =
+  xs
+    .filter(_.sum == 2020)
+      .map(_.product)
+        .toSet
 
 @main def hello: Unit = 
   val lines = Source.fromFile("input.txt").getLines.toList
 
   val intLines = lines.map(_.toInt)
 
-  // product(product(lines, lines), lines).filter((xy, z) => xy._1.toInt + xy._2.toInt + z.toInt == 2020) foreach println
+  val nestedLines = intLines.map(List(_))
 
-  product(intLines, intLines)
-    .filter((x, y) => x + y == 2020)
-      .map((x, y) => x * y) 
-        .toSet
-          .foreach(i => println("Day 1: " + i))
+  solve(cross(nestedLines, nestedLines))
+    .foreach(i => println("Day 1: " + i))
 
-  product(product(intLines, intLines), intLines)
-    .filter((xy, z) => xy._1 + xy._2 + z == 2020)
-      .map((xy, z) => xy._1 * xy._2 * z)
-        .toSet
-          .foreach(i => println("Day 2: " + i))
+  solve(cross(cross(nestedLines, nestedLines), nestedLines))
+    .foreach(i => println("Day 2: " + i))
 
